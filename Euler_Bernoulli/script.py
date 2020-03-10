03ff8cf52b7ff313d62d3399b804cbe060702702
romarowski <romarowski@wisc.edu>
2020-03-10 16:24:24 -0300

    euler bernoulli working'

--- Euler_Bernoulli/script.py
+++ Euler_Bernoulli/script.py
@@ -1,10 +1,67 @@
-from Euler_Bernoulli import *                                           
+from Euler_Bernoulli import *               
+
 from scipy import linalg
 
-K = stiffnes(21)                                                        
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+K = stiffnes(21)                           
 
 f = nodal_load(1, 21)                                           
+#f = np.zeros(40)
+#f[20] =  1
+
 
 d = linalg.solve(K, f)                                                  
+S = stress_recovery(d, 21)
 
-S = stress_recovery(d, 21)     
+
+
+
+M = np.max(np.abs(S))
+
+xnode = np.linspace(0, 1, 22)
+
+displ_w = d[0:40:2] #where 40 is the number of unconstrained nodes
+
+displ_w = np.concatenate([[0], displ_w, [0]])
+
+linear_loads = f[0:40:2]
+#moment_loads = f[1:40:2]
+
+#simple plot for verification
+plt.figure(1)
+plt.plot(xnode, np.zeros(22))
+plt.plot(xnode, np.zeros(22), '*')
+plt.plot(xnode, displ_w)
+plt.show()
+
+#stress recovery by averaging on the nodes
+
+#stress = np.zeros(22)
+
+#stress[0]  = S[0, 0]
+#stress[-1] = S[-1, 1]
+stress = S
+
+
+
+#for i in range(0, np.size(S, 0) - 1):
+   # stress[i + 1] = (S[i, 1] + S[i + 1, 0]) / 2
+
+
+#xsensor = xnode[14] #location of strain gauge
+
+xstress = np.linspace(1 / 42, 1 - 1 / 42, 21)
+
+plt.figure(2)
+plt.plot(xnode, np.zeros(22))
+plt.plot(xnode, np.zeros(22), '*')
+plt.plot(xstress, stress, '*')
+plt.show()
+
+strain_gauge = displ_w[14]
+
+
+
--- Euler_Bernoulli/stiffnes.py
+++ Euler_Bernoulli/stiffnes.py
@@ -1,29 +1,41 @@
-#code for setting up stifnnes matrix of euler Bernoulli beam
+def stiffnes(n_elem):
 
-import numpy as np
-#import matplotlib.plot as plt
+    #code for setting up stifnnes matrix of euler Bernoulli beam
 
-E  =  1
-Iz =  1
-L  =  1
+    import numpy as np
+    import sys
 
-k = np.array([[12 * E * Iz / L ** 3, 6 * E * Iz / L ** 2,\
-              -12 * E * Iz / L ** 3, 6 * E * Iz / L ** 2],
-	     [  6 * E * Iz / L ** 2, 4 * E * Iz / L,\
-               -6 * E * Iz / L ** 2, 2 * E * Iz / L],
-             [-12 * E * Iz / L ** 3,-6 * E * Iz / L ** 2,\
-               12 * E * Iz / L ** 3,-6 * E * Iz / L ** 2],
-             [  6 * E * Iz / L ** 2, 2 * E * Iz / L,\
-               -6 * E * Iz / L ** 2 ,4 * E * Iz / L]])
 
-dof_node = 2
-dof_elem = 4
-n_elem   = 21
-tot_dofs = n_elem * dof_node + 2
+    #np.set_printoptions(threshold=sys.maxsize)
+    #import matplotlib.plot as plt
 
-k_struct = np.zeros([tot_dofs,tot_dofs])
+    fixed_dofs = [1, 0, -2, -1]
+    E  =  1
+    Iz =  1
+    L  =  1
 
-for i in np.arange(0, tot_dofs, 3):
-    k_struct[(np.arange(i+3), np.arange(i+3))] += k
+    k = np.array([[12 * E * Iz / L ** 3, 6 * E * Iz / L ** 2,\
+                  -12 * E * Iz / L ** 3, 6 * E * Iz / L ** 2],
+                 [  6 * E * Iz / L ** 2, 4 * E * Iz / L,\
+                   -6 * E * Iz / L ** 2, 2 * E * Iz / L],
+                 [-12 * E * Iz / L ** 3,-6 * E * Iz / L ** 2,\
+                   12 * E * Iz / L ** 3,-6 * E * Iz / L ** 2],
+                 [  6 * E * Iz / L ** 2, 2 * E * Iz / L,\
+                   -6 * E * Iz / L ** 2 ,4 * E * Iz / L]])
 
-print(k_struct)
+    dof_node = 2
+    dof_elem = 4
+    #n_elem   = 21
+    tot_dofs = n_elem * dof_node + 2
+
+    k_struct = np.zeros([tot_dofs,tot_dofs])
+
+    for i in range(n_elem):
+        k_struct[2*i:2*i+4, 2*i:2*i+4] += k
+
+    for dof in fixed_dofs:
+        for i in [0, 1]: 
+            k_struct = np.delete(k_struct, dof, axis = i)
+
+
+    return  k_struct
