89c96c13be819633aceaa8f75c5617194f7d8fb0
romarowski <romarowski@wisc.edu>
2020-03-10 16:23:47 -0300

    up to date'

--- Euler_Bernoulli/Euler_Bernoulli.py
+++ Euler_Bernoulli/Euler_Bernoulli.py
@@ -69,6 +69,22 @@ def nodal_load(load, n_elem):
     return f 
 
 
+def random_load_generator(n_elem):
+    import random
+    import numpy as np
+
+    n_nod = n_elem + 1
+    n_dofs = n_nod * 2
+    free_dofs = n_dofs - 4
+    f = np.zeros(free_dofs)
+    for i in range(0, 40, 2):
+        if random.choice([True, False]):
+            f[i] = random.random()   
+    
+    return f
+
+
+
 def stress_recovery(displ, n_elem):
 
     import numpy as np
@@ -80,15 +96,20 @@ def stress_recovery(displ, n_elem):
     B_0 = np.array([-6 / L ** 2, - 4 / L, 6 / L ** 2, - 2 / L]) #stress-deformation vector
     B_1 = B_0 + np.array([12 / L ** 3, 6 / L ** 2, - 12 / L ** 3, 6 / L ** 2])
 
-    stress_elem = np.zeros([n_elem, 2])
+    B_x = lambda x: np.array([- 6 / L ** 2 + 12 * x / L ** 3, \
+                              - 4 / L      +  6 * x / L ** 2, \
+                                6 / L ** 2 - 12 * x / L ** 3, \
+                              - 2 / L      +  6 * x / L ** 2])
+    
+    stress_elem = np.zeros([n_elem])
 
 
     displ = np.concatenate(([0, 0], displ, [0, 0]))
 
     for elem in range(n_elem):
-        stress_elem[elem, 0] = - B_0.transpose() @ displ[2*elem:2*elem+4]
-        stress_elem[elem, 1] = - B_1.transpose() @ displ[2*elem:2*elem+4]
-
+       # stress_elem[elem, 0] = - B_0.transpose() @ displ[2*elem:2*elem+4]
+       # stress_elem[elem, 1] = - B_1.transpose() @ displ[2*elem:2*elem+4]
+       stress_elem[elem] = - B_x(L / 2) @ displ[2*elem:2*elem+4]
 
 
     return stress_elem
