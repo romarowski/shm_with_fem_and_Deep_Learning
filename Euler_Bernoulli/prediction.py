f83d1019a026727f849f4675a1e92f37ad022ecb
romarowski <romarowski@wisc.edu>
2020-03-10 16:23:00 -0300

    prediction working for Euler Bernoulli beam

---
+++ Euler_Bernoulli/prediction.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from sklearn import linear_model 
+
+from sklearn.metrics import r2_score
+
+from data_gen import simulation
+
+data = simulation(21)
+
+xtrain = data[:, 0]
+xtrain = xtrain[:, np.newaxis]
+
+ytrain = data[:, 1]
+ytrain = ytrain[:, np.newaxis]
+
+reg = linear_model.LinearRegression()
+
+reg.fit(xtrain, ytrain)
+
+data2 = simulation(21)
+
+xpredict = data2[:, 0]
+xpredict = xpredict[:, np.newaxis]
+
+ypredict = reg.predict(xpredict)
+
+ytest = data2[:, 1]
+ytest = ytest[:, np.newaxis]
+
+r2 = r2_score(ytest, ypredict)
+
