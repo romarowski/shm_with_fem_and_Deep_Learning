a3f6ee7116cf4158c216926bfdf043532ce07d51
romarowski <romarowski@wisc.edu>
2020-03-13 11:59:46 -0300

    working example

---
+++ Euler_Bernoulli/data_gen.py
@@ -0,0 +1,25 @@
+def simulation(n_elem):    
+    
+    from Euler_Bernoulli import stress_recovery, random_load_generator, stiffnes 
+
+    from scipy import linalg
+
+    import numpy as np
+
+
+    K = stiffnes(n_elem)
+
+    loc_sg = 14 #strain gauge located at node 14
+
+    data = np.zeros([5000,2])
+
+    for i in range (0, 5000):
+        f = random_load_generator(n_elem)
+        d = linalg.solve(K, f)
+        S = stress_recovery(d, n_elem)
+        M = np.max(np.abs(S))
+        sg = d[loc_sg * 2 - 2]
+        data[i, 0] = sg
+        data[i, 1] = M
+
+    return data
