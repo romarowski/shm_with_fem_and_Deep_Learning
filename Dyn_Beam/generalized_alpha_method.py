226f12610d81fbe796686a31b16655f480e5a968
romarowski <romarowski@wisc.edu>
2020-04-02 17:24:32 -0300

    seem to be working fine

--- Dyn_Beam/generalized_alpha_method.py
+++ Dyn_Beam/generalized_alpha_method.py
@@ -1,7 +1,7 @@
 
- #this is a time advancing scheme for an euler-bernoulli beam
+#this is a time advancing scheme for an euler-bernoulli beam
 
- #ref: https://fenicsproject.org/docs/dolfin/latest/python/demos/elastodynamics/demo_elastodynamics.py.html
+#ref: https://fenicsproject.org/docs/dolfin/latest/python/demos/elastodynamics/demo_elastodynamics.py.html
 
  
 def advance(t_sim, timestep, n_elem,  M, C, K):
@@ -21,7 +21,7 @@ def advance(t_sim, timestep, n_elem,  M, C, K):
 
     alpha_m = 0.2
     alpha_f = 0.4
-    gamma = .5 + alpha_m - alpha_f
+    gamma = .5 - alpha_m + alpha_f
     beta = .25 * (gamma + .5 ) ** 2
 
     c1 = gamma * (1 - alpha_f) / (beta * h)
@@ -39,35 +39,41 @@ def advance(t_sim, timestep, n_elem,  M, C, K):
     displ = np.zeros([dofs, nbr_timesteps])
     vel   = np.zeros([dofs, nbr_timesteps])
     acel  = np.zeros([dofs, nbr_timesteps])
- 
-    for i in np.arange(0, np.size(times) - 1):
+     
+    for i in np.arange(0, np.size(times) - 1):#-1 because np.arange works like [a, b) i.e. b-1 is the last included
         
-        u = displ[:, i]
+        d = displ[:, i]
         v =   vel[:, i]
         a =  acel[:, i]
-
-
-        lhs = F[:, i] - alpha_f * K @ u - C @ ( c1 * u + c2 * v + c3 * a) - \
-                M @ (m1 * u + m2 * v + m3 * a)
         
-
-        u_new = LA.solve(K_bar, lhs)
-        displ[:, i+1] = u_new
         
-        a_new = update_a(u_new, u, v, a, beta, h)
+        rhs = F[:, i] - alpha_f * K @ d - C @ ( c1 * d + c2 * v + c3 * a) \
+                      - M @ (m1 * d  + m2 * v + m3 * a)
+        
+        try: 
+            d_new = LA.solve(K_bar, rhs)
+        except:
+            ipdb.set_trace()
+        
+        displ[:, i+1] = d_new
+        
+        a_new = update_a(d_new, d, v, a, beta, h)
         acel[:, i+1] = a_new
 
-        v_new = update_v(a_new, u, v, a, gamma, h)
+        v_new = update_v(a_new, v, a, gamma, h)
         vel[:, i+1] = v_new
         #ipdb.set_trace()
 
-    return displ, vel, acel, F
+    return displ, F
 
 def update_a(u, u_old, v_old, a_old, beta, h):
     return (u-u_old-h*v_old)/beta/h**2-(1-2*beta)/2/beta*a_old
 
-def update_v(a, u_old, v_old, a_old, gamma, h):
+def update_v(a, v_old, a_old, gamma, h):
     return v_old + h*((1-gamma)*a_old + gamma*a)
+
+def update_d(u_old, v_old, a_old, a_new, beta, h):
+    return u_old + h * v_old + h ** 2 * ((.5 - beta) * a_old + beta * a_new)
 
 def avg(x_old, x_new, alpha):
     return alpha * x_old + (1 - alpha) * x_new
@@ -83,24 +89,21 @@ def loads_vector(n_elem, times, alpha_f):
     
     nbr_timesteps = np.size(times)
 
-    F = np.zeros([dofs, nbr_timesteps])
+    #F = np.zeros([dofs, nbr_timesteps])
 
     # load = sin(t) at each node
 
-    
-    loads = np.array([np.sin(times), ] * dofs)
-    
-    
 
     for i in np.arange(0, np.size(times) - 1):
-        f_old = loads[:, i]
-        f_new = loads[:, i+1]
+        t_old = times[i]
+        t_new = times[i+1]
 
 
-        F[:, i+1] = avg(f_old, f_new, alpha_f)
+        times[i+1] = avg(t_old, t_new, alpha_f)
 
+    loads = np.array([np.sin(times), ] * dofs)
 
-    return F
+    return loads
 
 
 
