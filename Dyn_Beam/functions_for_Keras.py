d8e35ef2da200fdd31df299d933315723569fbb8
romarowski <romarowski@wisc.edu>
2020-04-08 11:34:14 -0300

    somewhat working still high loss

---
+++ Dyn_Beam/functions_for_Keras.py
@@ -0,0 +1,110 @@
+import ipdb
+import numpy as np
+from matplotlib import pyplot as plt
+
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batch_size=128, step=6):
+    import numpy as np
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(
+                min_index + lookback, max_index, size=batch_size)
+    else:
+        if i + batch_size >= max_index:
+            i = min_index + lookback
+        rows = np.arange(i, min(i + batch_size, max_index))
+        i += len(rows)
+    samples = np.zeros((len(rows),
+                        lookback // step,
+                        data.shape[-1]))
+    targets = np.zeros((len(rows),))
+    for j, row in enumerate(rows):
+        indices = range(rows[j] - lookback, rows[j], step)
+        samples[j] = data[indices]
+        targets[j] = data[rows[j] + delay][1]
+    yield samples, targets
+
+
+
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                      target_size, step, single_step=False):
+  data = []
+  labels = []
+
+  start_index = start_index + history_size
+  if end_index is None:
+    end_index = len(dataset) - target_size
+
+  #ipdb.set_trace()
+  for i in range(start_index, end_index):
+    indices = range(i-history_size, i, step)
+    data.append(dataset[indices])
+
+    if single_step:
+      labels.append(target[i+target_size])
+    else:
+      labels.append(target[i:i+target_size])
+
+  return np.array(data), np.array(labels)
+
+
+
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+  data = []
+  labels = []
+
+  start_index = start_index + history_size
+  if end_index is None:
+    end_index = len(dataset) - target_size
+
+  for i in range(start_index, end_index):
+    indices = range(i-history_size, i)
+    # Reshape data from (history_size,) to (history_size, 1)
+    data.append(np.reshape(dataset[indices, 0], (history_size, 1)))
+    labels.append(dataset[i+target_size, 1])
+  return np.array(data), np.array(labels)
+
+
+def plot_train_history(history, title):
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  epochs = range(len(loss))
+
+  plt.figure()
+
+  plt.plot(epochs, loss, 'b', label='Training loss')
+  plt.plot(epochs, val_loss, 'r', label='Validation loss')
+  plt.title(title)
+  plt.legend()
+
+  plt.show()
+
+
+def show_plot(plot_data, delta, title):
+  labels = ['History', 'True Future', 'Model Prediction']
+  marker = ['.-', 'rx', 'go']
+  time_steps = create_time_steps(plot_data[0].shape[0])
+  if delta:
+    future = delta
+  else:
+    future = 0
+
+  plt.title(title)
+  for i, x in enumerate(plot_data):
+    if i:
+      plt.plot(future, plot_data[i], marker[i], markersize=10,
+               label=labels[i])
+    else:
+      plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+  plt.legend()
+  plt.xlim([time_steps[0], (future+5)*2])
+  plt.xlabel('Time-Step')
+  return plt
+
+
+def create_time_steps(length):
+  return list(range(-length, 0))
